c83c992f602ac4bd73fa5b809cebbe3147b2af96
Mohsin Ali <mohsinalisep@gmail.com>
2021-10-06 23:05:53 +0500

    Updates the models

--- app/project/models.py
+++ app/project/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from django import forms
 from django.db.models.base import Model
 
-
+# You might want to remove this and attach your other models directly to the default User model
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
@@ -14,10 +14,9 @@ class UserProfile(models.Model):
     def __str__(self):
         return self.user.username
 
-
+# Model for teacher must hold data only related specifically to the Teacher
 class Teacher(models.Model):
     teacher_name = models.OneToOneField(UserProfile, on_delete=models.CASCADE)
-    task_details = models.FileField()
 
     class Meta:
         verbose_name = 'Teacher'
@@ -26,27 +25,32 @@ class Teacher(models.Model):
     def __str__(self):
         return self.teacher_name
 
-
-class Assignment(models.Model):
-    assignment_crator = models.ForeignKey(Teacher, on_delete=models.CASCADE)
-    upload_assignment = models.FileField(null=True)
-    marks_obtained = models.FloatField(default=0.0)
-
-    class Meta:
-        verbose_name = 'Assignment'
-        verbose_name_plural = 'Assignments'
-
-
 class Student(models.Model):
     student_name = models.OneToOneField(UserProfile,
                                         on_delete=models.CASCADE,
                                         null=True)
     student_class = models.CharField(null=True)
-    assignment_upload = models.ForeignKey(Assignment,
-                                          on_delete=models.CASCADE,
-                                          null=True,
-                                          blank=True)
+
 
     class Meta:
         verbose_name = 'Student'
         verbose_name_plural = 'Students'
+
+
+# This holds all the grading and stuff for a submission of an assignment
+class Submissions(models.Model):
+    submitted_by = models.OneToOneField(Student, null=False)
+    submission_file = models.FileField(null=False)
+    marks_obtained = models.FloatField(default=0.0)
+    
+
+class Assignment(models.Model):
+    assignment_creator = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+    upload_assignment = models.FileField(null=True)
+    task_details = models.FileField(null=True)
+    
+    class Meta:
+        verbose_name = 'Assignment'
+        verbose_name_plural = 'Assignments'
+
+
